2c5c81df730c8eddb7428161d837fb9e227d2995
Shadow0x86 <shadow0x86@gmail.com>
2018-11-14 23:53:04 +0800

    BaseBinarySearchTree

--- trees.h
+++ trees.h
@@ -6,6 +6,9 @@
 namespace lyf
 {
 
+	template<typename Valt, typename Node>
+	class _BaseBinarySearchTree;
+
 	template<typename Valt, typename BaseNode>
 	class BinarySearchTree;
 
@@ -50,9 +53,19 @@ namespace lyf
 			_parent = _left = _right = nullptr;
 		}
 
+		void set_neighbors(nodeptr np)
+		{
+			_parent = _left = _right = np;
+		}
+
 		using _MyBase::_MyBase;
 
 		_BaseBSTNode() {}
+
+		_BaseBSTNode(nodeptr np)
+			: _parent(np), _left(np), _right(np)
+		{
+		}
 
 		_BaseBSTNode(const _BaseBSTNode &rhs)
 			: _MyBase(rhs), _parent(), _left(), _right()
@@ -66,13 +79,15 @@ namespace lyf
 		typename _CheckedNodeContainer<BSTNode<Valt, Base>>::nodeptr>
 	{
 		friend class _CheckedNodeContainer<BSTNode>;
+		friend class _BaseBinarySearchTree<Valt, BSTNode>;
 		friend class BinarySearchTree<Valt, Base>;
 
 	public:
-		using nodeptr = typename _CheckedNodeContainer<BSTNode>::nodeptr;
+		using nodeptr = typename _CheckedNodeContainer<BSTNode<Valt, Base>>::nodeptr;
 		using _MyBase = _BaseBSTNode<Valt, Base, nodeptr>;
 
 	private:
+		inline static nodeptr const _pNullNode = nullptr;
 		unsigned char _same_flag = 0;
 
 		void reverse_flag()
@@ -82,7 +97,10 @@ namespace lyf
 
 		using _MyBase::_MyBase;
 
-		BSTNode() {}
+		BSTNode()
+			: _MyBase(_pNullNode)
+		{
+		}
 
 		BSTNode(const BSTNode &rhs)
 			: _MyBase(rhs), _same_flag()
@@ -91,19 +109,153 @@ namespace lyf
 	};
 
 
+	template<typename Valt, typename Node>
+	class _BaseBinarySearchTree : public _CheckedNodeContainer<Node>
+	{
+	public:
+		using value_type = Valt;
+		using _MyBase = _CheckedNodeContainer<Node>;
+		using check_t = _CheckedNodeContainer<Node>;
+		using nodeptr = typename check_t::nodeptr;
+
+	protected:
+		_BaseBinarySearchTree()
+			: _root(Node::_pNullNode), _size()
+		{
+		}
+
+		_BaseBinarySearchTree(const _BaseBinarySearchTree &) = delete;
+		_BaseBinarySearchTree &operator=(const _BaseBinarySearchTree &) = delete;
+
+	public:
+		size_t size() const
+		{
+			return _size;
+		}
+
+		bool empty() const
+		{
+			return size() == 0;
+		}
+
+		nodeptr root() const
+		{
+			return this->_conv_null_np(_root);
+		}
+
+		nodeptr search(const value_type &value) const
+		{
+			nodeptr np = _root;
+			while (np != Node::_pNullNode)
+			{
+				if (np->value() == value)
+					break;
+				else if (np->value() < value)
+					np = np->_right;
+				else
+					np = np->_left;
+			}
+			return this->_conv_null_np(np);
+		}
+
+		// The node of maximum value in subtree which rooted at the given node(default root)
+		nodeptr max_node(nodeptr np = nullptr) const
+		{
+			if (!np || np == Node::_pNullNode)
+				np = _root;
+			if (np != Node::_pNullNode)
+			{
+				this->_ensureInTree(np);
+				while (np->_right != Node::_pNullNode)
+					np = np->_right;
+			}
+			return this->_conv_null_np(np);
+		}
+
+		// The maximum value in subtree which rooted at the given node(default root)
+		value_type max_value(nodeptr np = nullptr) const
+		{
+			return this->max_node(np)->value();
+		}
+
+		// The node of minimum value in subtree which rooted at the given node(default root)
+		nodeptr min_node(nodeptr np = nullptr) const
+		{
+			if (!np || np == Node::_pNullNode)
+				np = _root;
+			if (np != Node::_pNullNode)
+			{
+				this->_ensureInTree(np);
+				while (np->_left != Node::_pNullNode)
+					np = np->_left;
+			}
+			return this->_conv_null_np(np);
+		}
+
+		// The minimum value in subtree which rooted at the given node(default root)
+		value_type min_value(nodeptr np = nullptr) const
+		{
+			return this->min_node(np)->value();
+		}
+
+		// The node with the smallest key greater than the key of the given node
+		nodeptr successor(nodeptr np) const
+		{
+			this->_ensureInTree(np);
+			if (np->_right != Node::_pNullNode)
+				return this->min_node(np->_right);
+			nodeptr p;
+			while ((p = np->_parent) != Node::_pNullNode && p->_left != np)
+				np = p;
+			return this->_conv_null_np(p);
+		}
+
+		// The node with the largest key less than the key of the given node
+		nodeptr predecessor(nodeptr np) const
+		{
+			this->_ensureInTree(np);
+			if (np->_left != Node::_pNullNode)
+				return this->max_node(np->_left);
+			nodeptr p;
+			while ((p = np->_parent) != Node::_pNullNode && p->_right != np)
+				np = p;
+			return this->_conv_null_np(p);
+		}
+
+	protected:
+		nodeptr _root = Node::_pNullNode;
+		size_t _size = 0;
+
+		void _ensureInTree(const nodeptr &np) const
+		{
+			np->_ensureInCont(this);
+		}
+
+		static void _set_neighbors_null(nodeptr np)
+		{
+			np->set_neighbors(Node::_pNullNode);
+		}
+
+		static nodeptr _conv_null_np(nodeptr np)
+		{
+			return np == Node::_pNullNode ? nullptr : np;
+		}
+	};
+
+
 	template<typename Valt, typename BaseNode = UniqueNode<Valt>>
-	class BinarySearchTree : public _CheckedNodeContainer<BSTNode<Valt, BaseNode>>
+	class BinarySearchTree : public _BaseBinarySearchTree<Valt, BSTNode<Valt, BaseNode>>
 	{
 	public:
 		using value_type = Valt;
 		using Node = BSTNode<Valt, BaseNode>;
-		using _MyBase = _CheckedNodeContainer<Node>;
+		using _MyBase = _BaseBinarySearchTree<Valt, BSTNode<Valt, BaseNode>>;
 		using check_t = _CheckedNodeContainer<Node>;
 		using nodeptr = typename check_t::nodeptr;
 
 	public:
 		BinarySearchTree()
-			: _MyBase(), _root(), _size()
+			: _MyBase()
 		{
 		}
 
@@ -130,13 +282,12 @@ namespace lyf
 		BinarySearchTree(BinarySearchTree &&rhs)
 			: _MyBase(std::move(rhs)), _root(std::move(rhs._root)), _size(rhs._size)
 		{
-			rhs._root = nullptr;
+			rhs._root = Node::_pNullNode;
 			rhs._size = 0;
 		}
 
 		BinarySearchTree &operator=(const BinarySearchTree &rhs)
 		{
-			_MyBase::operator=(rhs);
 			if (this != &rhs)
 			{
 				this->clear();
@@ -147,13 +298,12 @@ namespace lyf
 
 		BinarySearchTree &operator=(BinarySearchTree &&rhs)
 		{
-			_MyBase::operator=(std::move(rhs));
 			if (this != &rhs)
 			{
 				this->_destroy_subtree();
 				_root = std::move(rhs._root);
 				_size = rhs._size;
-				rhs._root = nullptr;
+				rhs._root = Node::_pNullNode;
 				rhs._size = 0;
 			}
 			return *this;
@@ -172,16 +322,6 @@ namespace lyf
 			return ret;
 		}
 
-		size_t size() const
-		{
-			return _size;
-		}
-
-		bool empty() const
-		{
-			return size() == 0;
-		}
-
 		void insert(const value_type &value)
 		{
 			this->_insert_node(new Node(this, value));
@@ -198,97 +338,13 @@ namespace lyf
 			this->_insert_node(new Node(this, std::forward<Types>(args)...));
 		}
 
-		nodeptr root() const
-		{
-			return _root;
-		}
-
-		nodeptr search(const value_type &value) const
-		{
-			nodeptr np = _root;
-			while (np)
-			{
-				if (np->value() == value)
-					break;
-				else if (np->value() < value)
-					np = np->_right;
-				else
-					np = np->_left;
-			}
-			return np;
-		}
-
-		// The node of maximum value in subtree which rooted at the given node(default root)
-		nodeptr max_node(nodeptr np = nullptr) const
-		{
-			if (!np)
-				np = _root;
-			if (np)
-			{
-				this->_ensureInTree(np);
-				while (np->_right)
-					np = np->_right;
-			}
-			return np;
-		}
-
-		// The maximum value in subtree which rooted at the given node(default root)
-		value_type max_value(nodeptr np = nullptr) const
-		{
-			return this->max_node(np)->value();
-		}
-
-		// The node of minimum value in subtree which rooted at the given node(default root)
-		nodeptr min_node(nodeptr np = nullptr) const
-		{
-			if (!np)
-				np = _root;
-			if (np)
-			{
-				this->_ensureInTree(np);
-				while (np->_left)
-					np = np->_left;
-			}
-			return np;
-		}
-
-		// The minimum value in subtree which rooted at the given node(default root)
-		value_type min_value(nodeptr np = nullptr) const
-		{
-			return this->min_node(np)->value();
-		}
-
-		// The node with the smallest key greater than the key of the given node
-		nodeptr successor(nodeptr np) const
-		{
-			this->_ensureInTree(np);
-			if (np->_right)
-				return this->min_node(np->_right);
-			nodeptr p;
-			while ((p = np->_parent) && p->_left != np)
-				np = p;
-			return p;
-		}
-
-		// The node with the largest key less than the key of the given node
-		nodeptr predecessor(nodeptr np) const
-		{
-			this->_ensureInTree(np);
-			if (np->_left)
-				return this->max_node(np->_left);
-			nodeptr p;
-			while ((p = np->_parent) && p->_right != np)
-				np = p;
-			return p;
-		}
-
 		void remove(nodeptr np)
 		{
 			this->_ensureInTree(np);
 			nodeptr new_np;
-			if (!(np->_left && np->_right))
+			if (np->_left == Node::_pNullNode && np->_right == Node::_pNullNode)
 			{
-				new_np = np->_left ? np->_left : np->_right;
+				new_np = np->_left != Node::_pNullNode ? np->_left : np->_right;
 			}
 			else
 			{
@@ -303,7 +359,7 @@ namespace lyf
 				new_np->_left = np->_left;
 				np->_left->_parent = new_np;
 			}
-			if (np->_parent)
+			if (np->_parent != Node::_pNullNode)
 			{
 				if (np->_parent->_left == np)
 					np->_parent->_left = new_np;
@@ -311,8 +367,8 @@ namespace lyf
 					np->_parent->_right = new_np;
 			}
 			else
-				_root = new_np;
-			if (new_np)
+				_root = new_np == nullptr ? Node::_pNullNode : new_np;
+			if (new_np != Node::_pNullNode)
 				new_np->_parent = np->_parent;
 
 			this->_delete_node(np);
@@ -323,7 +379,7 @@ namespace lyf
 		bool remove(const value_type &value)
 		{
 			nodeptr np = this->search(value);
-			if (!np)
+			if (!np || np == Node::_pNullNode)
 				return false;
 			this->remove(np);
 			return true;
@@ -340,12 +396,12 @@ namespace lyf
 		{
 			this->_ensureInTree(np);
 			nodeptr right = np->_right;
-			if (!right)
+			if (right == Node::_pNullNode)
 				return;
 			np->_right = right->_left;
-			if (right->_left)
+			if (right->_left != Node::_pNullNode)
 				right->_left->_parent = np;
-			if (!(np->_parent))
+			if (np->_parent == Node::_pNullNode)
 				_root = right;
 			else if (np == np->_parent->_left)
 				np->_parent->_left = right;
@@ -362,12 +418,12 @@ namespace lyf
 		{
 			this->_ensureInTree(np);
 			nodeptr left = np->_left;
-			if (!left)
+			if (left == Node::_pNullNode)
 				return;
 			np->_left = left->_right;
-			if (left->_right)
+			if (left->_right != Node::_pNullNode)
 				left->_right->_parent = np;
-			if (!(np->_parent))
+			if (np->_parent == Node::_pNullNode)
 				_root = left;
 			else if (np == np->_parent->_right)
 				np->_parent->_right = left;
@@ -379,20 +435,13 @@ namespace lyf
 		}
 
 	private:
-		nodeptr _root = nullptr;
-		size_t _size = 0;
-
-		void _ensureInTree(const nodeptr &np) const
-		{
-			np->_ensureInCont(this);
-		}
 
 		void _insert_node(Node *pNode)
 		{
 			nodeptr np = this->_new_node(pNode);
 			const value_type &npv = np->value();
-			nodeptr x = _root, y = nullptr;
-			while (x)
+			nodeptr x = _root, y = Node::_pNullNode;
+			while (x != Node::_pNullNode)
 			{
 				y = x;
 				if (x->value() < npv)
@@ -406,7 +455,7 @@ namespace lyf
 				}
 			}
 			np->_parent = y;
-			if (!y)
+			if (y == Node::_pNullNode)
 				_root = np;
 			else if (y->value() < npv)
 				y->_right = np;
@@ -424,16 +473,16 @@ namespace lyf
 
 		static void _copy_subtree_recursive(nodeptr dst_np, nodeptr src_np, size_t &size)
 		{
-			while (src_np)
+			while (src_np != Node::_pNullNode)
 			{
-				if (src_np->_left)
+				if (src_np->_left != Node::_pNullNode)
 				{
 					dst_np->_left = check_t::_new_node(new Node(*(src_np->_left)));
 					dst_np->_left->_parent = dst_np;
 					dst_np->_left->_setCont(&dst_np);
 					size++;
 				}
-				if (src_np->_right)
+				if (src_np->_right != Node::_pNullNode)
 				{
 					dst_np->_right = check_t::_new_node(new Node(*(src_np->_right)));
 					dst_np->_right->_parent = dst_np;
@@ -448,12 +497,12 @@ namespace lyf
 
 		static void _copy_subtree(BinarySearchTree &dst, const BinarySearchTree &src, nodeptr np)
 		{
-			if (!np)
+			if (!np || np == Node::_pNullNode)
 				throw std::runtime_error("Subtree's root can't be null");
 			src._ensureInTree(np);
 			dst._destroy_subtree();
 			nodeptr curr;
-			if (np)
+			if (np != Node::_pNullNode)
 			{
 				curr = check_t::_new_node(new Node(*np));
 				dst._root = curr;
@@ -466,7 +515,7 @@ namespace lyf
 		void _destroy_subtree_recursive(nodeptr np)
 		{
 			nodeptr right;
-			while (np)
+			while (np != Node::_pNullNode)
 			{
 				_destroy_subtree_recursive(np->_left);
 				right = np->_right;
@@ -478,17 +527,17 @@ namespace lyf
 
 		void _destroy_subtree(nodeptr np = nullptr)
 		{
-			if (!np)
+			if (!np || np == Node::_pNullNode)
 				np = _root;
-			if (np && np->_parent)
+			if (np != Node::_pNullNode && np->_parent != Node::_pNullNode)
 			{
 				if (np->_parent->_left == np)
-					np->_parent->_left = nullptr;
+					np->_parent->_left = Node::_pNullNode;
 				else
-					np->_parent->_right = nullptr;
+					np->_parent->_right = Node::_pNullNode;
 			}
 			else
-				_root = nullptr;
+				_root = Node::_pNullNode;
 			_destroy_subtree_recursive(np);
 		}
 	};
@@ -550,7 +599,7 @@ namespace lyf
 
 	public:
 		RedBlackTree()
-			: _MyBase(), _root(_NULL_NODE), _size()
+			: _MyBase(), _root(_pNullNode), _size()
 		{
 		}
 
@@ -577,7 +626,7 @@ namespace lyf
 		RedBlackTree(RedBlackTree &&rhs)
 			: _MyBase(std::move(rhs)), _root(std::move(rhs._root)), _size(rhs._size)
 		{
-			rhs._root = _NULL_NODE;
+			rhs._root = _pNullNode;
 			rhs._size = 0;
 		}
 
@@ -600,7 +649,7 @@ namespace lyf
 				this->_destroy_subtree();
 				_root = std::move(rhs._root);
 				_size = rhs._size;
-				rhs._root = _NULL_NODE;
+				rhs._root = _pNullNode;
 				rhs._size = 0;
 			}
 			return *this;
@@ -653,7 +702,7 @@ namespace lyf
 		nodeptr search(const value_type &value) const
 		{
 			nodeptr np = _root;
-			while (np != _NULL_NODE)
+			while (np != _pNullNode)
 			{
 				if (np->value() == value)
 					break;
@@ -668,12 +717,12 @@ namespace lyf
 		// The node of maximum value in subtree which rooted at the given node(default root)
 		nodeptr max_node(nodeptr np = nullptr) const
 		{
-			if (!np || np == _NULL_NODE)
+			if (!np || np == _pNullNode)
 				np = _root;
-			if (np != _NULL_NODE)
+			if (np != _pNullNode)
 			{
 				this->_ensureInTree(np);
-				while (np->_right != _NULL_NODE)
+				while (np->_right != _pNullNode)
 					np = np->_right;
 			}
 			return this->_conv_null_np(np);
@@ -688,12 +737,12 @@ namespace lyf
 		// The node of minimum value in subtree which rooted at the given node(default root)
 		nodeptr min_node(nodeptr np = nullptr) const
 		{
-			if (!np || np == _NULL_NODE)
+			if (!np || np == _pNullNode)
 				np = _root;
-			if (np != _NULL_NODE)
+			if (np != _pNullNode)
 			{
 				this->_ensureInTree(np);
-				while (np->_left != _NULL_NODE)
+				while (np->_left != _pNullNode)
 					np = np->_left;
 			}
 			return this->_conv_null_np(np);
@@ -709,10 +758,10 @@ namespace lyf
 		nodeptr successor(nodeptr np) const
 		{
 			this->_ensureInTree(np);
-			if (np->_right != _NULL_NODE)
+			if (np->_right != _pNullNode)
 				return this->min_node(np->_right);
 			nodeptr p;
-			while ((p = np->_parent) != _NULL_NODE && p->_left != np)
+			while ((p = np->_parent) != _pNullNode && p->_left != np)
 				np = p;
 			return this->_conv_null_np(p);
 		}
@@ -721,10 +770,10 @@ namespace lyf
 		nodeptr predecessor(nodeptr np) const
 		{
 			this->_ensureInTree(np);
-			if (np->_left != _NULL_NODE)
+			if (np->_left != _pNullNode)
 				return this->max_node(np->_left);
 			nodeptr p;
-			while ((p = np->_parent) != _NULL_NODE && p->_right != np)
+			while ((p = np->_parent) != _pNullNode && p->_right != np)
 				np = p;
 			return this->_conv_null_np(p);
 		}
@@ -735,7 +784,7 @@ namespace lyf
 		bool remove(const value_type &value)
 		{
 			nodeptr np = this->search(value);
-			if (!np || np == _NULL_NODE)
+			if (!np || np == _pNullNode)
 				return false;
 			this->remove(np);
 			return true;
@@ -752,12 +801,12 @@ namespace lyf
 		{
 			this->_ensureInTree(np);
 			nodeptr right = np->_right;
-			if (right == _NULL_NODE)
+			if (right == _pNullNode)
 				return;
 			np->_right = right->_left;
-			if (right->_left != _NULL_NODE)
+			if (right->_left != _pNullNode)
 				right->_left->_parent = np;
-			if (np->_parent == _NULL_NODE)
+			if (np->_parent == _pNullNode)
 				_root = right;
 			else if (np == np->_parent->_left)
 				np->_parent->_left = right;
@@ -774,12 +823,12 @@ namespace lyf
 		{
 			this->_ensureInTree(np);
 			nodeptr left = np->_left;
-			if (left == _NULL_NODE)
+			if (left == _pNullNode)
 				return;
 			np->_left = left->_right;
-			if (left->_right != _NULL_NODE)
+			if (left->_right != _pNullNode)
 				left->_right->_parent = np;
-			if (np->_parent == _NULL_NODE)
+			if (np->_parent == _pNullNode)
 				_root = left;
 			else if (np == np->_parent->_right)
 				np->_parent->_right = left;
@@ -791,19 +840,19 @@ namespace lyf
 		}
 
 	private:
-		nodeptr _root = _NULL_NODE;
+		nodeptr _root = _pNullNode;
 		size_t _size = 0;
 
-		static nodeptr const _NULL_NODE;
+		static nodeptr const _pNullNode;
 
 		static void _set_null_neighbors(nodeptr np)
 		{
-			np->_set_neighbors(_NULL_NODE);
+			np->_set_neighbors(_pNullNode);
 		}
 
 		static nodeptr _conv_null_np(nodeptr np)
 		{
-			return np == _NULL_NODE ? nullptr : np;
+			return np == _pNullNode ? nullptr : np;
 		}
 
 		void _ensureInTree(const nodeptr &np) const
@@ -816,6 +865,6 @@ namespace lyf
 
 	template<typename Valt, typename BaseNode>
 	typename RedBlackTree<Valt, BaseNode>::nodeptr const 
-		RedBlackTree<Valt, BaseNode>::_NULL_NODE = typename RedBlackTree<Valt, BaseNode>::check_t::_new_node(
+		RedBlackTree<Valt, BaseNode>::_pNullNode = typename RedBlackTree<Valt, BaseNode>::check_t::_new_node(
 			new RedBlackTree<Valt, BaseNode>::Node);
 }
